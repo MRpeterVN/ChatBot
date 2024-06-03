ee1097a5651a899b03dc881505858d9566539c15
Lê Trường An <anlt.21it@vku.udn.vn>
2024-06-03 22:21:18 +0700

    init project

---
+++ app/src/main/java/com/ahmedapps/geminichatbot/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.ahmedapps.geminichatbot.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF466856)
+val PurpleGrey80 = Color(0xFF77A779)
+val Pink80 = Color(0xFF290202)
+
+val Purple40 = Color(0xFF1E3014)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Green = Color(0xFF4aa76f)
